6983b26963949468304746d2ccecbb886c4f444d
BEKKNO\torbjorntessem <torbjorn.tessem@bekk.no>
2019-06-06 15:03:22 +0200

    First commit

---
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/EventHandlerApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.personbruker.dittnav.eventhandler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EventHandlerApplication
+
+fun main(args: Array<String>) {
+	runApplication<EventHandlerApplication>(*args)
+}
---
+++ src/test/kotlin/no/nav/personbruker/dittnav/eventhandler/EventHandlerApplicationTests.kt
@@ -0,0 +1,16 @@
+package no.nav.personbruker.dittnav.eventhandler
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class EventHandlerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

42fe8a74e47f909fd1bc733affbf0f78832ec2d4
BEKKNO\torbjorntessem <torbjorn.tessem@bekk.no>
2019-06-11 09:56:07 +0200

    Skrevet om fra å bruke spring til ktor

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/EventHandlerApplication.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/EventHandlerApplication.kt
@@ -1,11 +1,32 @@
 package no.nav.personbruker.dittnav.eventhandler
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.DefaultHeaders
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
-@SpringBootApplication
 class EventHandlerApplication
 
-fun main(args: Array<String>) {
-	runApplication<EventHandlerApplication>(*args)
+fun main(args: Array<String>){
+	val jsonResponse = """{
+    "ping": "pong"
+}"""
+
+	embeddedServer(Netty, 8080) {
+		install(DefaultHeaders) {
+			header("X-Developer", "Team Personbruker")
+			header(HttpHeaders.Server, "My Server")
+		}
+		install(Routing) {
+			get("/ping") {
+				call.respondText(jsonResponse, ContentType.Application.Json)
+			}
+		}
+	}.start(wait = true)
 }
--- src/test/kotlin/no/nav/personbruker/dittnav/eventhandler/EventHandlerApplicationTests.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/eventhandler/EventHandlerApplicationTests.kt
@@ -1,12 +1,7 @@
 package no.nav.personbruker.dittnav.eventhandler
 
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
 class EventHandlerApplicationTests {
 
 	@Test

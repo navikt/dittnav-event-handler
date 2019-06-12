41efc4f77e131da5dd642026678786ae5ac2918e
Torbjørn Tessem <torbjorn.tessem@nav.no>
2019-06-12 09:20:21 +0200

    Ryddet logikk ut fra Main til egne funksjoner for Server og helsesjekk

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/EventHandlerApplication.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/EventHandlerApplication.kt
@@ -1,32 +1,5 @@
 package no.nav.personbruker.dittnav.eventhandler
 
-import io.ktor.application.call
-import io.ktor.application.install
-import io.ktor.features.DefaultHeaders
-import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
-import io.ktor.response.respondText
-import io.ktor.routing.Routing
-import io.ktor.routing.get
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
-
-class EventHandlerApplication
-
 fun main(args: Array<String>){
-	val jsonResponse = """{
-    "ping": "pong"
-}"""
-
-	embeddedServer(Netty, 8080) {
-		install(DefaultHeaders) {
-			header("X-Developer", "Team Personbruker")
-			header(HttpHeaders.Server, "My Server")
-		}
-		install(Routing) {
-			get("/ping") {
-				call.respondText(jsonResponse, ContentType.Application.Json)
-			}
-		}
-	}.start(wait = true)
+	Server.startServer(System.getenv("PORT")?.toInt() ?: 8080).start(wait = true)
 }

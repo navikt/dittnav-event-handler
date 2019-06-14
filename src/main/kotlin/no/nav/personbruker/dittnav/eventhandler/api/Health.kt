9de50e6f32b43fabcc73d9c68ea628c1276df894
Vanessa Kerr <Vanessa.kjolner.kerr@nav.no>
2019-06-14 15:22:17 +0200

    Leser ut hemmelighet (env) fra Vault. Lagt til link i ingress så vi kan nå appen fra laptop.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/api/Health.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/api/Health.kt
@@ -9,6 +9,7 @@ import io.ktor.routing.get
 fun Routing.healthApi() {
 
     val pingJsonResponse = """{"ping": "pong"}"""
+    var vaultEnvironmentName : String = System.getenv("hemmelig_key") ?: "default_value"
 
     get("/isAlive") {
         call.respondText(text = "ALIVE", contentType = ContentType.Text.Plain)
@@ -20,5 +21,9 @@ fun Routing.healthApi() {
 
     get("/ping"){
         call.respondText(pingJsonResponse,ContentType.Application.Json)
+    }
+
+    get("/vault_env"){
+        call.respondText(text = vaultEnvironmentName, contentType = ContentType.Text.Plain)
     }
 }

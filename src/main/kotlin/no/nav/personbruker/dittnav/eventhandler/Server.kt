b0932b8c1e097a5d5c38270378236ce7f3f0c43c
Torbjørn Tessem <torbjorn.tessem@nav.no>
2019-06-24 14:52:43 +0200

    [PB-99] Henter token fra request, laget skall for validering og henter ut ident for kall mot cache/db.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/Server.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/Server.kt
@@ -1,30 +1,16 @@
 package no.nav.personbruker.dittnav.eventhandler
 
-import com.auth0.jwk.JwkProviderBuilder
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import no.nav.personbruker.dittnav.eventhandler.api.healthApi
 import no.nav.personbruker.dittnav.eventhandler.api.regelApi
-import java.net.URL
 import java.util.concurrent.TimeUnit
 
 object Server {
-
-    val disableJwt = false
-    var jwkUrl = "https://security-token-service.nais.adeo.no/rest/v1/sts/jwks"
-    val jwkIssuer = "https://security-token-service.nais.adeo.no"
-    val jwkProvider = JwkProviderBuilder(URL(jwkUrl))
-            .cached(10, 24, TimeUnit.HOURS)
-            .rateLimited(10, 1, TimeUnit.MINUTES)
-            .build()
-
     fun start() {
         val app = embeddedServer(Netty, port = 8080) {
-            regelApi (jwkIssuer,
-                    jwkProvider,
-                    disableJwt)
-            routing { healthApi() }
+            routing { healthApi(); regelApi() }
         }
         app.start(wait = false)
         Runtime.getRuntime().addShutdownHook(Thread {
--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/api/Sikkerhet.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/api/Sikkerhet.kt
@@ -1,44 +1,45 @@
 package no.nav.personbruker.dittnav.eventhandler.api
 
-import com.auth0.jwk.JwkProvider
-import io.ktor.application.Application
+import com.auth0.jwt.JWT
+import com.auth0.jwt.interfaces.DecodedJWT
 import io.ktor.application.call
-import io.ktor.application.install
-import io.ktor.auth.Authentication
-import io.ktor.auth.authenticate
-import io.ktor.auth.jwt.JWTPrincipal
-import io.ktor.auth.jwt.jwt
-import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.ApplicationRequest
 import io.ktor.response.respondText
+import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.routing.routing
 
-val jwkRealm = "dittnav-event-handler"
+fun Routing.regelApi() {
+    get("/sikkerhet") {
+        val authToken : String? = getAuthToken(call.request)
 
-fun Application.regelApi(
-        jwtIssuer: String,
-        jwkProvider: JwkProvider,
-        disableJwt: Boolean = false
-) {
-    install(DefaultHeaders)
-
-    install(Authentication) {
-        jwt {
-            verifier(jwkProvider, jwtIssuer)
-            realm = jwkRealm
-            validate { credentials ->
-                println(credentials.payload.subject.toString() + " -authenticated-")
-                JWTPrincipal(credentials.payload)
-            }
+        if(validateToken(authToken)){
+            val ident :String  = getIdFromToken(authToken)
+            call.respondText(text = "OK" , contentType = ContentType.Text.Plain)
         }
-    }
-
-    routing {
-        authenticate(optional = disableJwt) {
-            get("/sikkerhet") {
-                call.respondText(text = "OK", contentType = ContentType.Text.Plain)
-            }
+        else{
+            call.response.status(HttpStatusCode.Unauthorized)
         }
     }
 }
+
+fun getAuthToken(request: ApplicationRequest): String?{
+    return request.headers.get(HttpHeaders.Authorization)
+}
+
+fun validateToken(authToken: String?): Boolean {
+    //TODO timestamp, skjekke mot azure?
+    return authToken != null
+}
+
+fun getIdFromToken(authToken: String?): String{
+    val decodedJWT : DecodedJWT = decodeToken(authToken)
+    return decodedJWT.getClaim("sub").asString()
+}
+
+fun decodeToken(authToken: String?): DecodedJWT{
+    return JWT.decode(authToken.toString().substring(7))
+}
+

a4df10ef759232acf69c25c611a7d06b17a6aa07
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2019-07-08 16:24:28 +0200

    * Gjort konfigurasjonen av appen lik slik som det er gjort for DittNAV-event-aggregator. * Appens rest-endepunkt returnerer nå JSON.
    PB-102. Skrive informasjon tilbake til Kafka

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/api/Fetch.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/api/Fetch.kt
@@ -1,12 +1,11 @@
 package no.nav.personbruker.dittnav.eventhandler.api
 
 import io.ktor.application.call
-import io.ktor.http.ContentType
-import io.ktor.response.respondText
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import kotlinx.coroutines.runBlocking
-import no.nav.personbruker.dittnav.eventhandler.database.entity.Informasjon
 import no.nav.personbruker.dittnav.eventhandler.service.InformasjonEventService
 import no.nav.personbruker.dittnav.eventhandler.util.AuthenticationUtil.authenticateRequest
 
@@ -18,21 +17,10 @@ fun Routing.fetchEventsApi() {
         authenticateRequest { ident ->
             runBlocking {
                 val events = informasjonEventService.getEventsFromCacheForUser(ident)
-                printOneEventPerLine(events)
-                call.respondText(text = events.toString(), contentType = ContentType.Application.Json)
+                call.respond(HttpStatusCode.OK, events)
             }
         }
 
     }
-
-}
-
-private fun printOneEventPerLine(events: List<Informasjon>) {
-    var counter = 0
-    for (event in events) {
-        counter++
-        println("Event $counter: $event")
-    }
-    println("Total number of events: $counter")
 
 }
--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/config/Server.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/config/Server.kt
@@ -1,8 +1,16 @@
 package no.nav.personbruker.dittnav.eventhandler.config
 
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.jackson.jackson
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.server.netty.NettyApplicationEngine
+import io.prometheus.client.hotspot.DefaultExports
 import no.nav.personbruker.dittnav.eventhandler.api.fetchEventsApi
 import no.nav.personbruker.dittnav.eventhandler.api.healthApi
 import no.nav.personbruker.dittnav.eventhandler.api.produceEventsApi
@@ -10,8 +18,22 @@ import no.nav.personbruker.dittnav.eventhandler.api.regelApi
 import java.util.concurrent.TimeUnit
 
 object Server {
-    fun start() {
-        val app = embeddedServer(Netty, port = 8080) {
+
+    const val portNumber = 8080
+
+    fun configure(): NettyApplicationEngine {
+        DefaultExports.initialize()
+        val app = embeddedServer(Netty, port = portNumber) {
+
+            install(DefaultHeaders)
+
+            install(ContentNegotiation) {
+                jackson {
+                    enable(SerializationFeature.INDENT_OUTPUT)
+                    registerModule(JavaTimeModule())
+                }
+            }
+
             routing {
                 healthApi()
                 regelApi()
@@ -19,9 +41,9 @@ object Server {
                 produceEventsApi()
             }
         }
-        app.start(wait = false)
         Runtime.getRuntime().addShutdownHook(Thread {
             app.stop(5, 60, TimeUnit.SECONDS)
         })
+        return app
     }
 }

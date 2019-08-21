4c69933e38fad8b786cea911bcd8f5d3edb7496c
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2019-08-21 15:07:27 +0200

    Lenke og URL kan sendes med i post-en og brukes i eventet som produseres på Kafka.
    PB-102. Skrive informasjon tilbake til Kafka

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/api/Produce.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/api/Produce.kt
@@ -2,6 +2,7 @@ package no.nav.personbruker.dittnav.eventhandler.api
 
 import io.ktor.application.call
 import io.ktor.http.ContentType
+import io.ktor.request.receive
 import io.ktor.response.respondText
 import io.ktor.routing.Route
 import io.ktor.routing.post
@@ -13,10 +14,17 @@ fun Route.produceEventsApi() {
     val producer = Producer
 
     post("/produce/informasjon") {
+        val postParametersDto = call.receive<ProduceDto>()
         val ident = extractIdentFromLoginContext()
-        producer.produceInformasjonEventForIdent(ident)
+        producer.produceInformasjonEventForIdent(ident, postParametersDto)
         val msg = "Et informasjons-event for identen: $ident har blitt lagt på kafka."
         call.respondText(text = msg, contentType = ContentType.Text.Plain)
     }
 
+}
+
+class ProduceDto(val tekst: String, val link: String) {
+    override fun toString(): String {
+        return "ProduceDto{tekst='$tekst', lenke='$link'}"
+    }
 }
--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/kafka/Producer.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/kafka/Producer.kt
@@ -1,6 +1,7 @@
 package no.nav.personbruker.dittnav.eventhandler.kafka
 
 import no.nav.personbruker.dittnav.event.schemas.Informasjon
+import no.nav.personbruker.dittnav.eventhandler.api.ProduceDto
 import no.nav.personbruker.dittnav.eventhandler.config.Environment
 import no.nav.personbruker.dittnav.eventhandler.config.Kafka
 import no.nav.personbruker.dittnav.eventhandler.config.Kafka.informasjonTopicName
@@ -13,22 +14,22 @@ object Producer {
 
     val log = LoggerFactory.getLogger(Producer::class.java)
 
-    fun produceInformasjonEventForIdent(ident: String) {
+    fun produceInformasjonEventForIdent(ident: String, dto: ProduceDto) {
         KafkaProducer<String, Informasjon>(Kafka.producerProps(Environment())).use { producer ->
-            producer.send(ProducerRecord(informasjonTopicName, createInformasjonForIdent(ident)))
+            producer.send(ProducerRecord(informasjonTopicName, createInformasjonForIdent(ident, dto)))
         }
         log.info("Har produsert et informasjons-event for identen: $ident")
     }
 
-    private fun createInformasjonForIdent(ident: String): Informasjon {
+    private fun createInformasjonForIdent(ident: String, dto: ProduceDto): Informasjon {
         val nowInMs = Instant.now().toEpochMilli()
         val build = Informasjon.newBuilder()
                 .setAktorId(ident)
                 .setDokumentId("100" + nowInMs)
                 .setEventId("" + nowInMs)
                 .setProdusent("DittNAV")
-                .setLink("https://nav.no/systemX/" + nowInMs)
-                .setTekst("Dette er informasjon til brukeren")
+                .setLink(dto.link)
+                .setTekst(dto.tekst)
                 .setTidspunkt(nowInMs)
                 .setSikkerhetsniva(4)
         return build.build()

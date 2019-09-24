a808de0b3e4670a2c58b94cb4002bd767d7953d0
Sissel Fladby <sissel.fladby@nav.no>
2019-09-24 16:36:37 +0200

    [PB-209] Legg inn håndtering av oppgaver

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/api/Fetch.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/api/Fetch.kt
@@ -7,14 +7,22 @@ import io.ktor.routing.Route
 import io.ktor.routing.get
 import no.nav.personbruker.dittnav.eventhandler.config.extractIdentFromLoginContext
 import no.nav.personbruker.dittnav.eventhandler.service.InformasjonEventService
+import no.nav.personbruker.dittnav.eventhandler.service.OppgaveEventService
 
 fun Route.fetchEventsApi() {
 
     val informasjonEventService = InformasjonEventService()
+    val oppgaveEventService = OppgaveEventService()
 
     get("/fetch/informasjon") {
         val ident = extractIdentFromLoginContext()
         val events = informasjonEventService.getEventsFromCacheForUser(ident)
+        call.respond(HttpStatusCode.OK, events)
+    }
+
+    get("/fetch/oppgave") {
+        val ident = extractIdentFromLoginContext()
+        val events = oppgaveEventService.getEventsFromCacheForUser(ident)
         call.respond(HttpStatusCode.OK, events)
     }
 

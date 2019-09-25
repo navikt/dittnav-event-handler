11255a6c4e5c0297cc7b97d0bc203ce0b9d475aa
Sissel Fladby <sissel.fladby@nav.no>
2019-09-25 14:17:12 +0200

    PB-209 Korrigering av oppgaveopprettelse og naming av sql-scripts

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/kafka/Producer.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/kafka/Producer.kt
@@ -1,6 +1,7 @@
 package no.nav.personbruker.dittnav.eventhandler.kafka
 
 import no.nav.brukernotifikasjon.schemas.Informasjon
+import no.nav.brukernotifikasjon.schemas.Oppgave
 import no.nav.personbruker.dittnav.eventhandler.api.ProduceDto
 import no.nav.personbruker.dittnav.eventhandler.config.Environment
 import no.nav.personbruker.dittnav.eventhandler.config.Kafka
@@ -17,21 +18,35 @@ object Producer {
 
     fun produceInformasjonEventForIdent(ident: String, dto: ProduceDto) {
         KafkaProducer<String, Informasjon>(Kafka.producerProps(Environment())).use { producer ->
-            producer.send(ProducerRecord(informasjonTopicName, createEventForIdent(ident, dto)))
+            producer.send(ProducerRecord(informasjonTopicName, createInformasjonForIdent(ident, dto)))
         }
         log.info("Har produsert et informasjons-event for identen: $ident")
     }
 
     fun produceOppgaveEventForIdent(ident: String, dto: ProduceDto) {
-        KafkaProducer<String, Informasjon>(Kafka.producerProps(Environment())).use { producer ->
-            producer.send(ProducerRecord(oppgaveTopicName, createEventForIdent(ident, dto)))
+        KafkaProducer<String, Oppgave>(Kafka.producerProps(Environment())).use { producer ->
+            producer.send(ProducerRecord(oppgaveTopicName, createOppgaveForIdent(ident, dto)))
         }
         log.info("Har produsert et oppgace-event for identen: $ident")
     }
 
-    private fun createEventForIdent(ident: String, dto: ProduceDto): Informasjon {
+    private fun createInformasjonForIdent(ident: String, dto: ProduceDto): Informasjon {
         val nowInMs = Instant.now().toEpochMilli()
         val build = Informasjon.newBuilder()
+                .setAktorId(ident)
+                .setDokumentId("100$nowInMs")
+                .setEventId("$nowInMs")
+                .setProdusent("DittNAV")
+                .setLink(dto.link)
+                .setTekst(dto.tekst)
+                .setTidspunkt(nowInMs)
+                .setSikkerhetsniva(4)
+        return build.build()
+    }
+
+    private fun createOppgaveForIdent(ident: String, dto: ProduceDto): Oppgave {
+        val nowInMs = Instant.now().toEpochMilli()
+        val build = Oppgave.newBuilder()
                 .setAktorId(ident)
                 .setDokumentId("100$nowInMs")
                 .setEventId("$nowInMs")

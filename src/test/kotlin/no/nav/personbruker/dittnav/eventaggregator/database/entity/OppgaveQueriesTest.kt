044c3a214c31492a527815b78a9fc84f565817b2
Sissel Fladby <sissel.fladby@nav.no>
2019-09-26 10:55:01 +0200

    [PB-209] Erstattet spek og assertj med junit og kluent

--- src/test/kotlin/no/nav/personbruker/dittnav/eventaggregator/database/entity/OppgaveQueriesTest.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/eventaggregator/database/entity/OppgaveQueriesTest.kt
@@ -3,33 +3,31 @@ package no.nav.personbruker.dittnav.eventaggregator.database.entity
 import kotlinx.coroutines.runBlocking
 import no.nav.personbruker.dittnav.eventaggregator.database.H2Database
 import no.nav.personbruker.dittnav.eventhandler.database.entity.getOppgaveByAktorid
-import org.assertj.core.api.Assertions.assertThat
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
+import org.junit.jupiter.api.Test
+import org.amshove.kluent.*
 
-object OppgaveQueriesTest : Spek({
+class OppgaveQueriesTest {
 
-    val database = H2Database()
+    private val database = H2Database()
 
-    describe("Returnerer cachede Oppgave-eventer") {
-        it("Finner cachede Oppgave-eventer for aktørID") {
-            runBlocking {
-                assertThat(database.dbQuery { getOppgaveByAktorid("12345") })
-                        .hasSize(2)
-            }
-
-        }
-        it("Returnerer tom liste hvis Oppgave-eventer for aktørID ikke finnes") {
-            runBlocking {
-                assertThat(database.dbQuery { getOppgaveByAktorid("finnesikke") })
-                        .isEmpty()
-            }
-        }
-        it("Returnerer tom liste hvis Oppgave-eventer hvis tom aktørID") {
-            runBlocking {
-                assertThat(database.dbQuery { getOppgaveByAktorid("") })
-                        .isEmpty()
-            }
+    @Test
+    fun `Finner cachede Oppgave-eventer for aktørID`() {
+        runBlocking {
+            database.dbQuery { getOppgaveByAktorid("12345") }.size `should be equal to` 2
         }
     }
-})
+
+    @Test
+    fun `Returnerer tom liste hvis Oppgave-eventer for aktørID ikke finnes`() {
+        runBlocking {
+            database.dbQuery { getOppgaveByAktorid("finnesikke") }.isEmpty()
+        }
+    }
+
+    @Test
+    fun `Returnerer tom liste hvis Oppgave-eventer hvis tom aktørID`() {
+        runBlocking {
+            database.dbQuery { getOppgaveByAktorid("") }.isEmpty()
+        }
+    }
+}

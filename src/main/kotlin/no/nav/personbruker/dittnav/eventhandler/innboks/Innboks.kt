95601609b5c7b9050fd09679c46e0ccf4c285684
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2019-11-25 17:19:32 +0100

    Rydder opp i JSON-serialiasering. * Sørger for at datoer serialiseres på et format som er leslig for mennesker. * Søger for at database ID-en per event ikke eksponeres ut, da dette kun er et internt anliggende. Det er eventID-er som vil bli brukt for å deaktivere eventer.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/innboks/Innboks.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/innboks/Innboks.kt
@@ -1,10 +1,11 @@
 package no.nav.personbruker.dittnav.eventhandler.innboks
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import no.nav.personbruker.dittnav.eventhandler.common.database.Brukernotifikasjon
 import java.time.ZonedDateTime
 
 data class Innboks (
-        override val id: Int,
+        @JsonIgnore override val id: Int,
         override val produsent: String,
         override val eventTidspunkt: ZonedDateTime,
         override val aktorId: String,

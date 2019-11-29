09f003aa48deab767b3de4147b8a9ba7596b6ed1
Christopher Santana <christopher.santana@nav.no>
2019-11-29 10:02:24 +0100

    Fjernet feilaktig bruk av runblocking.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/innboks/InnboksEventService.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/innboks/InnboksEventService.kt
@@ -1,20 +1,14 @@
 package no.nav.personbruker.dittnav.eventhandler.innboks
 
-import kotlinx.coroutines.runBlocking
 import no.nav.personbruker.dittnav.eventhandler.common.database.Database
 
 class InnboksEventService(private val database: Database) {
 
-    fun getCachedActiveEventsForUser(fodselsnummer: String): List<Innboks> {
-        return runBlocking {
-            database.dbQuery { getActiveInnboksByFodselsnummer(fodselsnummer) }
-        }
+    suspend fun getCachedActiveEventsForUser(fodselsnummer: String): List<Innboks> {
+        return database.dbQuery { getActiveInnboksByFodselsnummer(fodselsnummer) }
     }
 
-    fun getAllCachedEventsForUser(fodselsnummer: String): List<Innboks> {
-        return runBlocking {
-            database.dbQuery {
-                getAllInnboksByFodselsnummer(fodselsnummer) }
-        }
+    suspend fun getAllCachedEventsForUser(fodselsnummer: String): List<Innboks> {
+        return database.dbQuery { getAllInnboksByFodselsnummer(fodselsnummer) }
     }
 }

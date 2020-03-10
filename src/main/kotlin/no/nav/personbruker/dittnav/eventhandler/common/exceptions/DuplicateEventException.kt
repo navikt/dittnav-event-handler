8d4bc1e8b9b71a4f06f3c17610fa06dbd4a3ddea
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-03-10 09:13:49 +0100

    Sørger for at exception-meldingen blir sendt videre.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/common/exceptions/DuplicateEventException.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/common/exceptions/DuplicateEventException.kt
@@ -2,4 +2,4 @@ package no.nav.personbruker.dittnav.eventhandler.common.exceptions
 
 import java.lang.Exception
 
-class DuplicateEventException(message: String) : Exception()
+class DuplicateEventException(message: String) : Exception(message)
--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/common/exceptions/NoEventsException.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/common/exceptions/NoEventsException.kt
@@ -2,4 +2,4 @@ package no.nav.personbruker.dittnav.eventhandler.common.exceptions
 
 import java.lang.Exception
 
-class NoEventsException(message: String): Exception()
+class NoEventsException(message: String): Exception(message)

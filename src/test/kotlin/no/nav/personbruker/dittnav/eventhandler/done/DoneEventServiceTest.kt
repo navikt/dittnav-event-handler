3e5cafee22a9d72d26002e2a334d7cd6a1c5f62a
Robert Larsen <robert.larsen@nav.no>
2020-04-03 12:49:02 +0200

    [PB-392] Fjerner Flyway, da vi ønsker at det bare er dittnav-event-aggregator som skal sørge for databasemigreringene. Flytter opprettelse av testdata vekk fra Flyway-script og inne i de testene de tilhører.

--- src/test/kotlin/no/nav/personbruker/dittnav/eventhandler/done/DoneEventServiceTest.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/eventhandler/done/DoneEventServiceTest.kt
@@ -3,23 +3,48 @@ package no.nav.personbruker.dittnav.eventhandler.done
 import Beskjed
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
+import no.nav.personbruker.dittnav.eventhandler.beskjed.BeskjedObjectMother
 import no.nav.personbruker.dittnav.eventhandler.beskjed.createBeskjed
+import no.nav.personbruker.dittnav.eventhandler.beskjed.deleteBeskjed
 import no.nav.personbruker.dittnav.eventhandler.common.database.H2Database
 import no.nav.personbruker.dittnav.eventhandler.common.exceptions.DuplicateEventException
 import no.nav.personbruker.dittnav.eventhandler.common.exceptions.NoEventsException
 import org.amshove.kluent.`should be equal to`
 import org.amshove.kluent.`should throw`
 import org.amshove.kluent.invoking
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import java.time.ZonedDateTime
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DoneEventServiceTest {
 
     private val database = H2Database()
     private val doneProducer = mockk<DoneProducer>()
     private val doneEventService = DoneEventService(database, doneProducer)
     private val fodselsnummer = "12345"
-    private val uid = "1"
+    private val uid = "11"
     private val eventId = "125"
+
+    private val beskjed1 = BeskjedObjectMother.createBeskjed(id = 1, eventId = "125", fodselsnummer = "12345",
+            synligFremTil = ZonedDateTime.now().plusHours(1), uid = "11", aktiv = true)
+
+    @BeforeAll
+    fun `populer tabellen med Beskjed-event`() {
+        runBlocking {
+            database.dbQuery { createBeskjed(listOf(beskjed1)) }
+        }
+    }
+
+    @AfterAll
+    fun `slett Beskjed-event fra tabellen`() {
+        runBlocking {
+            database.dbQuery { deleteBeskjed(listOf(beskjed1)) }
+        }
+    }
+
 
     @Test
     fun `Kaster exception hvis listen er tom`() {
@@ -33,8 +58,8 @@ class DoneEventServiceTest {
 
     @Test
     fun `Kaster exception hvis det er duplikat i listen`() {
-        val beskjedListDuplicate = listOf<Beskjed>(createBeskjed(1, "dummyEventId1", "dummmyFnr1", null, "dummyUid1", true),
-                                                                createBeskjed(1, "dummyEventId1", "dummyFnr1", null, "dummyUid1", true))
+        val beskjedListDuplicate = listOf<Beskjed>(BeskjedObjectMother.createBeskjed(1, "dummyEventId1", "dummmyFnr1", null, "dummyUid1", true),
+                                                                BeskjedObjectMother.createBeskjed(1, "dummyEventId1", "dummyFnr1", null, "dummyUid1", true))
         invoking {
             runBlocking {
                 doneEventService.isEventBeskjedListValid(beskjedListDuplicate)

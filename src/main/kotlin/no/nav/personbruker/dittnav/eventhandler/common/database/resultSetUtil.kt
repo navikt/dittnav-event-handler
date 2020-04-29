2a3f3f16639deb306de6ab710e5eb8458e3fb39d
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-04-29 12:53:44 +0200

    Forsøker å konvertere ugyldige datoer til gyldig dato. Enkelte team har brukt epoch sekuner i stede for epoch millisekuner i feltet for eventtidspunkt. Her blir dette forsøkt rettet hvis tidspunktet er før 1975, som uansett er dato er som ikke er aktuelle for en typisk Oppgave. Oppgaver ble lansert i april 2020.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/common/database/resultSetUtil.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/common/database/resultSetUtil.kt
@@ -1,6 +1,9 @@
 package no.nav.personbruker.dittnav.eventhandler.common.database
 
 import java.sql.ResultSet
+import java.sql.Timestamp
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 fun <T> ResultSet.map(result: ResultSet.() -> T): List<T> =
         mutableListOf<T>().apply {
@@ -8,3 +11,16 @@ fun <T> ResultSet.map(result: ResultSet.() -> T): List<T> =
                 add(result())
             }
         }
+
+const val YEAR_LOWER_LIMIT = 1975
+val timeZone = ZoneId.of("Europe/Oslo")
+
+fun convertFromEpochSecondsToEpochMillis(originalTimestamp: Timestamp): ZonedDateTime {
+    var convertedDateTime = ZonedDateTime.ofInstant(originalTimestamp.toInstant(), timeZone)
+    if (convertedDateTime.year < YEAR_LOWER_LIMIT) {
+        val millis = originalTimestamp.toInstant().toEpochMilli()
+        val rawTimestampAsMillis = millis * 1000 * 1000
+        convertedDateTime = ZonedDateTime.ofInstant(Timestamp(rawTimestampAsMillis).toInstant(), timeZone)
+    }
+    return convertedDateTime
+}
---
+++ src/test/kotlin/no/nav/personbruker/dittnav/eventhandler/common/database/resultSetUtilKtTest.kt
@@ -0,0 +1,31 @@
+package no.nav.personbruker.dittnav.eventhandler.common.database
+
+import org.amshove.kluent.`should be equal to`
+import org.junit.jupiter.api.Test
+import java.sql.Timestamp
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+internal class resultSetUtilKtTest {
+
+    @Test
+    fun `should convert invalid dates`() {
+        val invalidTimestamp: Long = 1561932
+        val rawTimestamp = Timestamp(invalidTimestamp)
+
+        val resultat = convertFromEpochSecondsToEpochMillis(rawTimestamp)
+
+        resultat.year `should be equal to` 2019
+    }
+
+    @Test
+    fun `should not convert valid dates`() {
+        val ldt = LocalDateTime.of(2020, 4, 10, 8, 0).toInstant(ZoneOffset.UTC)
+        val rawTimestamp = Timestamp(ldt.toEpochMilli())
+
+        val resultat = convertFromEpochSecondsToEpochMillis(rawTimestamp)
+
+        resultat.year `should be equal to` 2020
+    }
+
+}

9cc5f8688922bf4c027447a0079deced102c0aae
Robert Larsen <robert.larsen@nav.no>
2020-06-10 09:49:50 +0200

    Flytter kode fra objectmother inn i en egen testqueries-fil.

---
+++ src/test/kotlin/no/nav/personbruker/dittnav/eventhandler/done/backupDoneTestQueries.kt
@@ -0,0 +1,33 @@
+package no.nav.personbruker.dittnav.eventhandler.done
+
+import java.sql.Connection
+import java.sql.Types
+
+fun Connection.createDoneInCache(done: List<BackupDone>) =
+        prepareStatement("""INSERT INTO done(systembruker, eventTidspunkt, fodselsnummer, eventId, grupperingsId)
+            VALUES(?, ?, ?, ?, ?)""")
+                .use {
+                    done.forEach { done ->
+                        run {
+                            it.setString(1, done.systembruker)
+                            it.setObject(2, done.eventTidspunkt.toLocalDateTime(), Types.TIMESTAMP)
+                            it.setString(3, done.fodselsnummer)
+                            it.setString(4, done.eventId)
+                            it.setString(5, done.grupperingsId)
+                            it.addBatch()
+                        }
+                    }
+                    it.executeBatch()
+                }
+
+fun Connection.deleteBackupDoneInCache(done: List<BackupDone>) =
+        prepareStatement("""DELETE FROM done WHERE eventId = ?""")
+                .use {
+                    done.forEach { done ->
+                        run {
+                            it.setString(1, done.eventId)
+                            it.addBatch()
+                        }
+                    }
+                    it.executeBatch()
+                }

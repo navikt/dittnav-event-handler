ce73eb2f92fd135c4b8f9833e0eeb1c8eac50f7c
Robert Larsen <robert.larsen@nav.no>
2021-09-16 09:48:18 +0200

    Tar i bruk siste versjon av dittnav-dependencies, med ny versjon av tms-ktor-token-support.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2021.08.31-14.26-eb38ce748636"
+val dittNavDependenciesVersion = "2021.09.16-09.20-bf15a56a82c6"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")

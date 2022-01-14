24a025966b047c60a1ba44095d30afe3f4a247a3
Christopher Santana <christopher.santana@nav.no>
2022-01-14 14:27:16 +0100

    Legg til @Serializable for EventCountForProducer.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/common/statistics/EventCountForProducer.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/common/statistics/EventCountForProducer.kt
@@ -1,5 +1,8 @@
 package no.nav.personbruker.dittnav.eventhandler.common.statistics
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class EventCountForProducer(
     val namespace: String,
     val appName: String,

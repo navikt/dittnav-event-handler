3f7709ccf6a316f1dba75d666cf5de6785fccbfe
Mauricio Burgos Herrera <mauricio.burgos.herrera@nav.no>
2022-01-25 11:23:01 +0100

    Fjern jcenter

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2021.09.16-09.20-bf15a56a82c6"
+val dittNavDependenciesVersion = "2022.01.21-08.52-0bbd2434111a"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")

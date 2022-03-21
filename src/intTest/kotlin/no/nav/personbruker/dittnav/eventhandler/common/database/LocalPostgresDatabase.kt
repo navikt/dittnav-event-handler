bc76d985d7c610ef3571bb4585369c4dca8f2bb6
Atle Nordland <atle.nordland@nav.no>
2022-03-21 11:12:21 +0100

    Fiks db-tester som feiler lokalt når det tar tid å kjøre opp testcontainer

--- src/intTest/kotlin/no/nav/personbruker/dittnav/eventhandler/common/database/LocalPostgresDatabase.kt
+++ src/intTest/kotlin/no/nav/personbruker/dittnav/eventhandler/common/database/LocalPostgresDatabase.kt
@@ -1,7 +1,11 @@
 package no.nav.personbruker.dittnav.eventhandler.common.database
 
 import com.zaxxer.hikari.HikariDataSource
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import org.postgresql.util.PSQLException
+import java.lang.Thread.sleep
 
 class LocalPostgresDatabase : Database {
 
@@ -29,8 +33,18 @@ class LocalPostgresDatabase : Database {
 
     private fun createTablesAndViews() {
         runBlocking {
-            val fileContent = this::class.java.getResource("/db/createTablesAndViews.sql").readText()
-            dbQuery { prepareStatement(fileContent).execute() }
+            withTimeout(3000) {
+                while (true) {
+                    try {
+                        val fileContent = this::class.java.getResource("/db/createTablesAndViews.sql").readText()
+                        dbQuery { prepareStatement(fileContent).execute() }
+                        return@withTimeout
+                    } catch (_: PSQLException) {
+                        delay(100)
+                    }
+                }
+            }
+
         }
     }
 }

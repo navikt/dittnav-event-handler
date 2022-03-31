2c539026b24ed91b19edc5e51989f4bed4306d70
Atle Nordland <atle.nordland@nav.no>
2022-03-31 14:32:47 +0200

    Bruk setString i sql

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/event/EventRepository.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/event/EventRepository.kt
@@ -7,50 +7,61 @@ import java.time.ZoneId
 import java.time.ZonedDateTime
 
 class EventRepository(private val database: Database) {
+
     suspend fun getInactiveEvents(fodselsnummer: String): List<Event> {
         return database.queryWithExceptionTranslation {
-            prepareStatement(
-                    """
-                    ${eventQuery(fodselsnummer, "beskjed")}
-                    UNION ALL
-                    ${eventQuery(fodselsnummer, "oppgave")}
-                    UNION ALL 
-                    ${eventQuery(fodselsnummer, "innboks")}
-                    """.trimIndent()).executeQuery().mapList {
+            prepareStatement("""
+                ${eventQuery("beskjed")}
+                UNION ALL
+                ${eventQuery("oppgave")}
+                UNION ALL 
+                ${eventQuery("innboks")}
+            """.trimIndent()
+            ).also {
+                it.setString(1, fodselsnummer)
+                it.setString(2, fodselsnummer)
+                it.setString(3, fodselsnummer)
+            }.executeQuery().mapList {
                 Event(
-                        fodselsnummer = getString("fodselsnummer"),
-                        grupperingsId = getString("grupperingsId"),
-                        eventId = getString("eventId"),
-                        eventTidspunkt = ZonedDateTime.ofInstant(getUtcTimeStamp("eventTidspunkt").toInstant(), ZoneId.of("Europe/Oslo")),
-                        produsent = getString("appnavn") ?: "",
-                        sikkerhetsnivaa = getInt("sikkerhetsnivaa"),
-                        sistOppdatert = ZonedDateTime.ofInstant(getUtcTimeStamp("sistOppdatert").toInstant(), ZoneId.of("Europe/Oslo")),
-                        tekst = getString("tekst"),
-                        link = getString("link"),
-                        aktiv = getBoolean("aktiv"),
-                        type = EventType.fromOriginalType(getString("type"))
+                    fodselsnummer = getString("fodselsnummer"),
+                    grupperingsId = getString("grupperingsId"),
+                    eventId = getString("eventId"),
+                    eventTidspunkt = ZonedDateTime.ofInstant(
+                        getUtcTimeStamp("eventTidspunkt").toInstant(),
+                        ZoneId.of("Europe/Oslo")
+                    ),
+                    produsent = getString("appnavn") ?: "",
+                    sikkerhetsnivaa = getInt("sikkerhetsnivaa"),
+                    sistOppdatert = ZonedDateTime.ofInstant(
+                        getUtcTimeStamp("sistOppdatert").toInstant(),
+                        ZoneId.of("Europe/Oslo")
+                    ),
+                    tekst = getString("tekst"),
+                    link = getString("link"),
+                    aktiv = getBoolean("aktiv"),
+                    type = EventType.fromOriginalType(getString("type"))
                 )
             }
         }
     }
 
-    private fun eventQuery(fodselsnummer: String, table: String): String {
+    private fun eventQuery(table: String): String {
         return """
-                SELECT
-                    eventTidspunkt,
-                    fodselsnummer,
-                    eventId,
-                    grupperingsId,
-                    tekst,
-                    appnavn,
-                    link,
-                    sikkerhetsnivaa,
-                    sistOppdatert,
-                    aktiv,
-                    '$table' as type 
-                FROM $table
-                WHERE fodselsnummer = '$fodselsnummer'
-                AND aktiv = false
-                """
+            SELECT
+                eventTidspunkt,
+                fodselsnummer,
+                eventId,
+                grupperingsId,
+                tekst,
+                appnavn,
+                link,
+                sikkerhetsnivaa,
+                sistOppdatert,
+                aktiv,
+                '$table' as type 
+            FROM $table
+            WHERE fodselsnummer = ?
+            AND aktiv = false
+        """
     }
 }

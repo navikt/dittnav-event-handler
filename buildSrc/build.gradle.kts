ae9c2acecc8af057a8af187a7a03853144a896c0
Atle Nordland <atle.nordland@nav.no>
2022-04-26 13:25:35 +0200

    Hent token-support-mocks fra dependencies-bibliotek

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2022.03.07-09.11-113dabef3447"
+val dittNavDependenciesVersion = "2022.04.26-13.19-0c38fa7d68db"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")

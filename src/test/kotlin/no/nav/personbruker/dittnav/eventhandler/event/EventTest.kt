02ce7c3dad5b5f84144e656a3955f55c2e83246e
Atle Nordland <atle.nordland@nav.no>
2022-07-01 11:23:48 +0200

    Test-refactoring: Enklere oppsett for generering av testdata

--- src/test/kotlin/no/nav/personbruker/dittnav/eventhandler/event/EventTest.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/eventhandler/event/EventTest.kt
@@ -1,96 +1,90 @@
 package no.nav.personbruker.dittnav.eventhandler.event
 
-import Beskjed
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.runBlocking
 import no.nav.personbruker.dittnav.eventhandler.beskjed.BeskjedObjectMother
 import no.nav.personbruker.dittnav.eventhandler.beskjed.createBeskjed
 import no.nav.personbruker.dittnav.eventhandler.common.database.LocalPostgresDatabase
-import no.nav.personbruker.dittnav.eventhandler.innboks.Innboks
 import no.nav.personbruker.dittnav.eventhandler.innboks.InnboksObjectMother
 import no.nav.personbruker.dittnav.eventhandler.innboks.createInnboks
-import no.nav.personbruker.dittnav.eventhandler.oppgave.Oppgave
 import no.nav.personbruker.dittnav.eventhandler.oppgave.OppgaveObjectMother
 import no.nav.personbruker.dittnav.eventhandler.oppgave.createOppgave
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import java.time.ZonedDateTime
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class EventTest {
     private val database = LocalPostgresDatabase.cleanDb()
 
     private val fodselsnummer = "12345678"
-    private val grupperingsid = "100${fodselsnummer}"
-    private val systembruker = "x-dittnav"
-    private val namespace = "localhost"
-    private val appnavn = "dittnav"
-
-    private val beskjed1 = BeskjedObjectMother.createBeskjed(
-            id = 1,
-            eventId = "9876",
-            fodselsnummer = fodselsnummer,
-            grupperingsId = grupperingsid,
-            synligFremTil = ZonedDateTime.now().plusHours(1),
-            aktiv = false,
-            systembruker = systembruker,
-            namespace = namespace,
-            appnavn = appnavn
-    )
-
-    private val oppgave1 = OppgaveObjectMother.createOppgave(
-            id = 1,
-            eventId = "234657",
-            fodselsnummer = fodselsnummer,
-            grupperingsId = grupperingsid,
-            aktiv = false,
-            systembruker = systembruker,
-            namespace = namespace,
-            appnavn = appnavn
-    )
-
-    private val innboks1 = InnboksObjectMother.createInnboks(
-            id = 1,
-            eventId = "123",
-            fodselsnummer = fodselsnummer,
-            grupperingsId = grupperingsid,
-            aktiv = false,
-            systembruker = systembruker,
-            namespace = namespace,
-            appnavn = appnavn
-    )
 
     @BeforeAll
     fun `populer testdata`() {
-        createBeskjed(listOf(beskjed1))
-        createOppgave(listOf(oppgave1))
-        createInnboks(listOf(innboks1))
+        createBeskjeder(3, 5)
+        createOppgaver(1, 4)
+        createInnboks(2, 1)
     }
 
     @Test
     fun `hente alle inaktive eventer for bruker`() {
         val eventRepository = EventRepository(database)
         runBlocking {
-            val inaktiveEventer = eventRepository.getInactiveEvents(beskjed1.fodselsnummer)
-            inaktiveEventer.size shouldBe 3
+            val inaktiveEventer = eventRepository.getInactiveEvents(fodselsnummer)
+            inaktiveEventer.size shouldBe 10
             inaktiveEventer.map { it.toEventDTO().type }.toSet() shouldBe setOf(EventType.BESKJED, EventType.OPPGAVE, EventType.INNBOKS)
         }
     }
 
-    private fun createBeskjed(beskjeder: List<Beskjed>) {
+    private fun createBeskjeder(antallAktive: Int, antallInaktive: Int) {
+        val beskjeder = (1..antallAktive).map {
+            BeskjedObjectMother.createBeskjed(
+                fodselsnummer = fodselsnummer,
+                aktiv = true
+            )
+        } + (1..antallInaktive).map {
+            BeskjedObjectMother.createBeskjed(
+                fodselsnummer = fodselsnummer,
+                aktiv = false
+            )
+        }
+
         runBlocking {
             database.dbQuery { createBeskjed(beskjeder) }
         }
     }
 
-    private fun createOppgave(oppgaver: List<Oppgave>) {
+    private fun createOppgaver(antallAktive: Int, antallInaktive: Int) {
+        val oppgaver = (1..antallAktive).map {
+            OppgaveObjectMother.createOppgave(
+                fodselsnummer = fodselsnummer,
+                aktiv = true
+            )
+        } + (1..antallInaktive).map {
+            OppgaveObjectMother.createOppgave(
+                fodselsnummer = fodselsnummer,
+                aktiv = false
+            )
+        }
+
         runBlocking {
             database.dbQuery { createOppgave(oppgaver) }
         }
     }
 
-    private fun createInnboks(innboks: List<Innboks>) {
+    private fun createInnboks(antallAktive: Int, antallInaktive: Int) {
+        val innboks = (1..antallAktive).map {
+            InnboksObjectMother.createInnboks(
+                fodselsnummer = fodselsnummer,
+                aktiv = true
+            )
+        } + (1..antallInaktive).map {
+            InnboksObjectMother.createInnboks(
+                fodselsnummer = fodselsnummer,
+                aktiv = false
+            )
+        }
+
         runBlocking {
             database.dbQuery { createInnboks(innboks) }
         }

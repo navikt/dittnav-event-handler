b48037872d4d42e64e8857927a5db7457ffb8f48
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-09-02 16:32:02 +0200

    debug logg for inaktivering av beskjed

--- src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/done/DoneEventService.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventhandler/done/DoneEventService.kt
@@ -4,12 +4,16 @@ import no.nav.personbruker.dittnav.eventhandler.beskjed.setBeskjedInaktiv
 import no.nav.personbruker.dittnav.eventhandler.common.database.Database
 import no.nav.personbruker.dittnav.eventhandler.statistics.EventCountForProducer
 import no.nav.tms.token.support.tokenx.validation.user.TokenXUser
+import org.slf4j.LoggerFactory
 
 class DoneEventService(private val database: Database) {
-
+    val log = LoggerFactory.getLogger(DoneEventService::class.java)
     suspend fun markEventAsInaktiv(innloggetBruker: TokenXUser, eventId: String) {
+        log.info("Forsøker å inaktivere $eventId")
         database.queryWithExceptionTranslation {
-            setBeskjedInaktiv(innloggetBruker.ident, eventId)
+            setBeskjedInaktiv(innloggetBruker.ident, eventId).also {
+                log.info("Satte $it beskjeder til inaktiv")
+            }
         }
     }
 
